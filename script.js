f1821658daffd4fd917e110c41efa312a2fd8689
Richet <shanon.richet@gmail.com>
2021-10-21 10:15:34 +0200

    add timer

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const compteur= document.querySelector('input')
+const score= document.querySelector('.score')
+const compteurDeClick= document.querySelector('.click')
 const cookie= document.querySelector('.cookie')
+const clock= document.querySelector('.timer')
+
+var timer= 0;
 var myValue= 0;
+var click= 0;
+
+cookie.addEventListener('click', clickCounter)
+function clickCounter(){
+compteurDeClick.value= click;
+click= click + 1;
+compteurDeClick.innerHTML= click;
+}
 
 cookie.onclick= () => {
-    compteur.value= myValue;
-    myValue = myValue + 2;
-    compteur.innerHTML = myValue;
-}
-
-while (myValue < 5){
-    cookie.onclick= () => {
-        compteur.value= myValue;
-        myValue = myValue + 5;
-        compteur.innerHTML = myValue;
+    score.value= myValue;
+    myValue= myValue + 1;
+    score.innerHTML= myValue;
+    if (myValue > 5){
+        cookie.onclick= () => {
+            score.value= myValue;
+            myValue= myValue + 2;
+            score.innerHTML= myValue;
+        }
     }
 }
+
+setInterval(() => {
+   clock.value= timer;
+   timer= timer + 1;
+   clock.innerHTML= timer;
+}, 1000);
