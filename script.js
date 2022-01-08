7f4384114fb93fb8c3273a8b65a843fee5d11e07
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2022-01-08 12:10:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,65 +5,70 @@ const cookie= document.querySelector('object')
 const clock= document.querySelector('.timer')
 const stored= localStorage.getItem('textinput')
 
-var timer= 0;
-var myValue= 0;
-var click= 0;
+var timer= 0
+var myValue= 0
+var click= 0
 var cookieImage= document.querySelector('object')
 if(localStorage.getItem('textinput') !== null){
     myValue= parseInt(localStorage.getItem('textinput'))
-    score.value= stored;
-    }
-    const saveToStorage= () => {
-        localStorage.setItem('textinput', score.value)
-    }
-    cookie.addEventListener('click', saveToStorage)
+    score.value= stored
+}
+const saveToStorage= () => {
+    localStorage.setItem('textinput', score.value)
+}
 
+cookie.addEventListener('click', saveToStorage)
 cookie.addEventListener('click', clickCounter)
-function clickCounter(){
-compteurDeClick.value= click;
-click= click + 1;
-compteurDeClick.innerHTML= click;
+
+function clickCounter() {
+compteurDeClick.value= click
+click= click + 1
+compteurDeClick.innerHTML= click
 } 
+
 cookie.addEventListener('click', cookieClicker)
+
 function cookieClicker() {
-    score.value= myValue;
-    myValue= myValue + 1;
-    score.innerHTML= myValue;
-    if (myValue > 5){
+    score.value= myValue
+    myValue= myValue + 1
+    score.innerHTML= myValue
+    if (myValue > 5) {
         cookie.onclick= () => {
-            score.value= myValue;
-            myValue= myValue + 2;
-            score.innerHTML= myValue;
+        score.value= myValue
+        myValue= myValue + 2
+        score.innerHTML= myValue
         }
     }
 }
+
 setInterval(() => {
-   clock.value= timer;
-   timer= timer + 1;
-   clock.innerHTML= timer;
+   clock.value= timer
+   timer= timer + 1
+   clock.innerHTML= timer
 }, 1000);
 
 setInterval(() => {
-    clickBefore= click;
-    let cps= 0;
-    let second= timer/ timer;
+    clickBefore= click
+    let cps= 0
+    let second= timer/ timer
     setTimeout(() => {
-        clickAfter= click;  
-    }, 999);
-    cps = + clickBefore - clickAfter;
+        clickAfter= click
+    }, 999)
+    
+    cps = + clickBefore - clickAfter
     var clickParSeconde= document.querySelector('.cps')
-    clickParSeconde.value= 'your cps: ' + cps;
-    clickParSeconde.innerHTML= 'your cps:' + cps; 
+    clickParSeconde.value= 'your cps: ' + cps
+    clickParSeconde.innerHTML= 'your cps:' + cps
     if (cps == 0){
         return;
     }
     if (cps >= 7){
-        myValue= myValue + 50;
+        myValue= myValue + 50
     }
     if (cps >= 9){
-        myValue= myValue + 100;
+        myValue= myValue + 100
     }
-    }, 1000);
+}, 1000)
 
 
 // Local Storage
